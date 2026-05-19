aa01051982dcc34679f79bbddd266920107230c5
monicavilhena <vilhenamonica58@gmail.com>
2026-05-18 22:42:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function comprar(produto) {
+    // Link do seu Google Forms (substitua depois pelo seu link real)
+    const linkFormulario = "https://forms.gle/https://forms.gle/eS9qDTQvYTV8Da4s7";
+
+    // Monta o link já preenchendo o nome do produto na URL
+    // (por enquanto vamos apenas abrir o formulário)
+    window.open(linkFormulario, "_blank");
+}
