b5f66124e44d77afb9a9f9257e122822905ceefe
monicavilhena <vilhenamonica58@gmail.com>
2026-08-03 18:34:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function comprar(produto) {
-    // Link do seu Google Forms (substitua depois pelo seu link real)
-    const linkFormulario = "https://forms.gle/https://forms.gle/eS9qDTQvYTV8Da4s7";
+function comprar(codigo) {
+    const linkFormulario = "https://docs.google.com/forms/d/e/1FAIpQLSe3gB-oiU73jU7yL_h-owlpu9C-2IngVhNG2P_XBsPvtWT6Yg/viewform?usp=header";
 
-    // Monta o link já preenchendo o nome do produto na URL
-    // (por enquanto vamos apenas abrir o formulário)
+    // Copia o código automaticamente
+    navigator.clipboard.writeText(codigo);
+
+    // Abre o formulário
     window.open(linkFormulario, "_blank");
+
+    // Mensagem para o cliente
+    alert("Código " + codigo + " copiado! Cole esse código no formulário.");
 }
